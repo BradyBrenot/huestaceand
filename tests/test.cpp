b37c91379e80655c631b3136f240bf361af2ac16
Brady Brenot <brady@brenot.ca>
2018-07-29 16:47:07 -0400

    add test for linking and connecting to bridge
    marking new "can connect to and link a hue bridge" test as non-default, and adding "interactive" tag. This requires user intervention and can't succeed in a purely-automated setting.

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,6 +7,7 @@
 #include <QSharedPointer>
 #include <QElapsedTimer>
 #include <QThread>
+#include <QTest>
 
 int main(int argc, char* argv[])
 {
@@ -71,4 +72,58 @@ TEST_CASE("server can start and stop", "") {
 	QSignalSpy stoppedSpy(server.data(), SIGNAL(finished()));
 	server->stop();
 	REQUIRE((stoppedSpy.count() == 1 || stoppedSpy.wait()));
+}
+
+TEST_CASE("can connect to and link a hue bridge", "[.][bridge][interactive]") {
+	//////////////////////////////////////////////////////////////////////////
+	// Startup
+	QSharedPointer<Huestaceand> daemon = QSharedPointer<Huestaceand>(new Huestaceand(nullptr), doDeleteLater);
+	daemon->listen(test_port++);
+
+	bool foundBridge = false;
+	WARN("looking for bridge");
+	for (int attempts = 0; attempts < 3; ++attempts)
+	{
+		WARN(3 - attempts);
+		foundBridge = daemon->getDeviceProviders().size() > 0;
+
+		if (foundBridge)
+		{
+			break;
+		}
+		else
+		{
+			QTest::qWait(1000);
+		}
+	}
+
+	REQUIRE(foundBridge);
+
+	auto deviceProviders = daemon->getDeviceProviders();
+	for (auto deviceProvider : deviceProviders)
+	{
+		CAPTURE(deviceProvider.first.toUtf8());
+		CAPTURE(deviceProvider.second->getName());
+		CAPTURE(deviceProvider.second->getState());
+		if (deviceProvider.second && deviceProvider.second->getState() == DeviceProvider::EDeviceState::PendingLink)
+		{
+			for (int attempts = 0; attempts < 10; ++attempts)
+			{
+				WARN("Device has not linked. Press the Link button on the device. Timeout in: " << (10 - attempts));
+				deviceProvider.second->link();
+				QTest::qWait(1000);
+
+				if (deviceProvider.second->getState() == DeviceProvider::EDeviceState::Connected)
+				{
+					break;
+				}
+			}
+		}
+
+		REQUIRE(deviceProvider.second->getState() == DeviceProvider::EDeviceState::Connected);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Shutdown
+	daemon->stop();
 }
